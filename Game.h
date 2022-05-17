ce36dab9bd60c0953626ca415c2a8d3efd7fbb56
Humaira Fasih <humairafasih18@gmail.com>
2022-05-18 04:19:14 +0500

    Added header file

---
+++ Game.h
@@ -0,0 +1,30 @@
+#ifndef GAME_H
+#define GAME_H
+
+#pragma once     // ensures this header is only ever declared once 
+
+class Animation {
+    public:
+        Animation();     // Animation constructor
+};
+
+class Game {
+    public:
+
+        // Public Functions
+        void StartGameArt();
+        void Choices_1();
+        void Choices_2();
+        void Choices_3();
+        void Choices_4();
+        void Choices_5();
+        void Choices_6();
+        void Choices_7();
+        void Choices_8();
+        void Choices_9();
+        void Choices_10();
+        void Choices_11();
+        void Choices_12();
+};
+
+#endif
